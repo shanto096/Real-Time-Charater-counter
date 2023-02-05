4cb8d61876cdb191c62f8d792e2925c09eef95ea
shanto096 <shanto096@gmail.com>
2023-02-06 03:18:52 +0600

    final

---
+++ script.js
@@ -0,0 +1,13 @@
+const input = document.getElementById('input');
+const total = document.getElementById('total');
+const remaining = document.getElementById('remaining');
+
+input.addEventListener("keyup", ()=>{
+    update()
+})
+
+function update() {
+   total.innerText = input.value.length;
+   remaining.innerText = input.getAttribute('maxlength') -input.value.length;
+}
+
